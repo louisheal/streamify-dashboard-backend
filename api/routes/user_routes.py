2dddb37724bf66b767ed9983f2f4a8995231abf9
Louis Heal <lh420@ic.ac.uk>
2023-11-10 17:20:10 +0000

    change root to post

--- api/routes/user_routes.py
+++ api/routes/user_routes.py
@@ -6,6 +6,6 @@ import api.controllers.user_controller as controller
 def get_user_data():
     return controller.get_user_data()
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def index():
     return controller.print_body()
